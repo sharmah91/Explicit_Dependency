d14b71168e1aa84bdd209bc606f551f79509bd99
Hitesh Sharma <sharmah91@gmail.com>
2025-05-27 15:48:48 +0530

    explicit dependency changes

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "5d5be31f-46ae-4693-866a-7cb188220117"
+  subscription_id = ""
 
 }
 
@@ -27,11 +27,3 @@ resource "azurerm_storage_account" "beti" {
   depends_on               = [azurerm_resource_group.meripyaaribeti]
 }
 
-resource "azurerm_storage_account" "beti1" {
-  name                     = "hmracc2"
-  location                 = azurerm_resource_group.meripyaaribeti.location
-  resource_group_name      = azurerm_resource_group.meripyaaribeti.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  depends_on               = [azurerm_resource_group.meripyaaribeti]
-}
